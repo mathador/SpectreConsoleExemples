71e271bc148f9933afd964d5ca8318a2014c39cc
Chris <christophe.machu@altran.com>
2025-09-29 23:40:57 +0200

    https://spectreconsole.net/markup

--- SpectreConsoleExemples/Program.cs
+++ SpectreConsoleExemples/Program.cs
@@ -1,3 +1,13 @@
 ﻿using Spectre.Console;
 
-AnsiConsole.Markup("[underline red]Hello[/] World!");
+AnsiConsole.Write(new Markup("[bold yellow]Hello[/] [red]World![/]"));
+AnsiConsole.WriteLine();
+
+
+var table = new Table();
+table.AddColumn(new TableColumn(new Markup("[yellow]Foo[/]")));
+table.AddColumn(new TableColumn("[blue]Bar[/]"));
+AnsiConsole.Write(table);
+
+
+AnsiConsole.MarkupLine("[link=https://spectreconsole.net]Spectre Console Documentation[/]");
